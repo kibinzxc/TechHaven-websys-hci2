616d1c929d9fc9b62da78a22586a8be3d3fb6c7e
mrknthny21 <markanthonybasmayor0416@gmail.com>
2024-06-19 23:54:49 +0800

    isolating sidenav and top

---
+++ costumer/profile.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   
+</head>
+    <style>
+        .mainbox {
+            border: black 1px solid;
+            height: 50vh;
+            width: 80%;
+            margin-left: 10vw;
+            margin-top: 10vh;
+            border-radius: 5px;
+            
+        }
+
+        .upper{
+            border-bottom: solid 1px black;
+            height: 22%;
+            width: 100%;
+
+        }
+
+       .right{
+            border-right: solid 1px black;
+            width: 70%;
+            height: 78%;
+
+       }
+    </style>
+<body>
+   
+<?php include 'sidenav.php'; ?>
+
+   <div class="mainbox">
+        <div class="upper">
+            
+        </div>
+        <div class="left">
+
+        </div>
+        <div class="right">
+
+        </div>
+   </div>
+    
+</body>
+
+</html>
--- costumer/sidenav.php
+++ costumer/sidenav.php
@@ -1,4 +1,90 @@
 <style>
+    * {
+    margin: 0;
+    box-sizing: border-box;
+    padding: 0;
+}
+
+:root {
+    --lightColor: #F5F5F5;
+    --shadedColor: #E7E7E7;
+    --darkModeNav: #292929;
+    --darkBody: #5C5C5C;
+}
+
+body.dark-mode {
+    background-color: var(--darkBody);
+    color: white;
+}
+.dark-mode .nav {
+    background-color: var(--darkModeNav);
+}
+.dark-mode .search-bar {
+    background-color: var(--lightColor);
+}
+.dark-mode .nav-right i {
+    color: var(--lightColor);
+}
+
+.rotate {
+    animation: rotate 0.5s ease-in-out;
+}
+@keyframes rotate {
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+}
+
+.nav {
+    background: var(--lightColor);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 4rem;
+    font: italic 600 20px/normal 'Inter';
+    box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.25);
+}
+.nav img {
+    width: 60px;
+}
+
+.nav-left, .nav-middle, .nav-right {
+    display: flex;
+    align-items: center;
+}
+
+.nav-middle {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+}
+
+.search-bar {
+    width: 100%;
+    max-width: 450px;
+    padding: 0.5px;
+    background-color: var(--shadedColor);
+    border-radius: 20px;
+    padding: 5px;
+    border: none;
+    outline: none;
+}
+
+.nav-right {
+    display: flex;
+    justify-content: space-between;
+    margin-right: 3%;
+}
+
+.nav-right i {
+    margin: 5px;
+    cursor: pointer;
+}
+
+
     .side-nav {
     position: fixed;
     margin-top: 7%;
@@ -70,7 +156,9 @@
     }
 }
 </style>
-
+<head>
+<link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
+</head>
 <body>
     <header class="nav">
         <div class="nav-left">
@@ -83,12 +171,12 @@
         <div class="nav-right">
             <i class="bi bi-moon-fill" id="dark-mode-toggle"></i>
             <i class="bi bi-cart3"></i>
-            <i class="bi bi-person-circle"></i>
+              <a href="profile.php"><i class="bi bi-person-circle"></i></a>
         </div>
     </header>
 
     <div class="side-nav">
-    <i class="bi bi-house-door-fill"></i> <a href="homepage.php">Home</a>
+   
     <i class="bi bi-box-seam-fill"><a href="products.php"></i> Products</a>
     <i class="bi bi-heart-fill"></i><a href="wishlist.php"> Wishlist</a>
     <i class="bi bi-cart-fill"></i><a href="cart.php"> Cart</a>
