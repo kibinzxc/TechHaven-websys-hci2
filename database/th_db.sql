0dedf74195874a4a7dd6bf8296369f73f2839ec8
Daniel <mamadaddyalexa@gmail.com>
2024-06-20 01:22:46 +0800

    database

--- database/th_db.sql
+++ database/th_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2024 at 02:13 AM
+-- Generation Time: Jun 19, 2024 at 07:20 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,6 +41,76 @@ CREATE TABLE `admin` (
 INSERT INTO `admin` (`adminID`, `name`, `email`, `password`) VALUES
 (1, 'Kevin Almirante', 'kevin.almirante@gmail.com', '202cb962ac59075b964b07152d234b70');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customerinfo`
+--
+
+CREATE TABLE `customerinfo` (
+  `customerID` int(100) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `contactNum` varchar(100) NOT NULL,
+  `address` varchar(250) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `feedbackID` int(100) NOT NULL,
+  `prod_name` varchar(100) NOT NULL,
+  `ratings` int(50) NOT NULL,
+  `customerID` int(100) NOT NULL,
+  `message` text NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`feedbackID`, `prod_name`, `ratings`, `customerID`, `message`, `date_created`) VALUES
+(1, 'RAKK Ilis RGB Mechanical Keyboard Gateron Yellow\r\n', 5, 20001, 'The product is in good condition. I love it!! I will order again soon', '2024-06-18 15:57:08'),
+(2, 'Rakk Aporo RGB Gaming Mouse', 3, 20001, 'The product is good however I saw some dents. The price is good', '2024-06-18 16:04:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `prodID` int(11) NOT NULL,
+  `categoryID` int(11) NOT NULL,
+  `prod_name` int(11) NOT NULL,
+  `prod_slogan` int(11) NOT NULL,
+  `prod_price` int(11) NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp(),
+  `date_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prod_inventory`
+--
+
+CREATE TABLE `prod_inventory` (
+  `invID` int(11) NOT NULL,
+  `prodID` int(100) NOT NULL,
+  `categoryID` int(100) NOT NULL,
+  `prod_name` varchar(250) NOT NULL,
+  `qty` int(250) NOT NULL,
+  `sold` int(100) NOT NULL,
+  `date_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -52,6 +122,30 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`adminID`);
 
 --
+-- Indexes for table `customerinfo`
+--
+ALTER TABLE `customerinfo`
+  ADD PRIMARY KEY (`customerID`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feedbackID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`prodID`);
+
+--
+-- Indexes for table `prod_inventory`
+--
+ALTER TABLE `prod_inventory`
+  ADD PRIMARY KEY (`invID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,6 +154,30 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `admin`
   MODIFY `adminID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `customerinfo`
+--
+ALTER TABLE `customerinfo`
+  MODIFY `customerID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20004;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `feedbackID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `prodID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `prod_inventory`
+--
+ALTER TABLE `prod_inventory`
+  MODIFY `invID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
